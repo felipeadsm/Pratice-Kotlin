1eaff4ef9be3653c3f2219481f48d27c9d1b8bbf
felipeadsm <97059009+felipeadsm@users.noreply.github.com>
2022-06-24 11:56:29 -0300

    update: conceitos de get e set em Kotlin

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,7 +2,9 @@ package one.digitalinnovation.digionebank
 
 class Pessoa {
     var nome: String = "Felipe"
+
     var cpf: String = "123.123.123-11"
+    private set
 
     inner class Endereco {
         var rua: String = "Rua Emilio Castelar"
@@ -12,8 +14,9 @@ class Pessoa {
 fun main(){
     val felipe = Pessoa()
 
+    felipe.cpf = "987.654.321-99"
+
     println(felipe.nome)
     println(felipe.cpf)
-
     println(felipe.Endereco().rua)
 }
