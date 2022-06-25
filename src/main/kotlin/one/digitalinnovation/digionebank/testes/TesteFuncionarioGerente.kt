0ecdd33cd856077759dac8adf5b33feb8173d841
felipeadsm <97059009+felipeadsm@users.noreply.github.com>
2022-06-25 17:47:45 -0300

    update: implementando interfaces

--- src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
@@ -3,6 +3,7 @@ package one.digitalinnovation.digionebank.testes
 import one.digitalinnovation.digionebank.Gerente
 
 fun main() {
-    val maria = Gerente("Maria João", "987.456.123-90",20000.00)
+    val maria = Gerente("Maria João", "987.456.123-90",20000.00, "senha123")
     ImrprimeRelatorioFuncionario.imprime(maria)
+    TesteAutenticacao().autentica(maria)
 }
