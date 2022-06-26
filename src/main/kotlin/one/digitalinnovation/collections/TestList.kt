529861efd0540b6eabd29fc2f4b248704fc67fe6
felipeadsm <97059009+felipeadsm@users.noreply.github.com>
2022-06-26 19:10:13 -0300

    update: introdução aos setof

--- src/main/kotlin/one/digitalinnovation/collections/TestList.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TestList.kt
@@ -15,15 +15,3 @@ fun main() {
 
     funcionarios.groupBy { it.tipoContrato }.forEach { println(it) }
 }
-
-data class Funcionario(
-    val nome: String,
-    val salario: Double,
-    val tipoContrato: String
-) {
-    override fun toString(): String = """
-        Nome: $nome
-        Salário: $salario
-        ------------------------------------------------------
-    """.trimIndent()
-}
